66782d4f9603e9b1bce324ee03fd6760d70b5068
Sebastian Göttsch <40594782+sgoettsch@users.noreply.github.com>
2024-06-17 21:20:07 +0200

    Add function to help with device types enum

--- src/FritzboxAHADeviceTypes.php
+++ src/FritzboxAHADeviceTypes.php
@@ -10,4 +10,14 @@ enum FritzboxAHADeviceTypes: int
     case FRITZ_DECT_300 = 320;
     case FRITZ_DECT_440 = 1048864;
     case FRITZ_DECT_500 = 237572;
+
+    public function getValue(): int
+    {
+        return match ($this) {
+            self::FRITZ_DECT_200 => 35712,
+            self::FRITZ_DECT_300 => 320,
+            self::FRITZ_DECT_440 => 1048864,
+            self::FRITZ_DECT_500 => 237572
+        };
+    }
 }
